bc2f12c6a957e2e21cc963ad7f033d6ef9b8009b
Pavel Petrov <popetrov@gmail.com>
2026-05-02 19:17:27 +0400

    update subscription page

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function goToPage(page) {
 function goHome() {
   window.location.href = "index.html";
 }
+
+function copyCardNumber() {
+  const cardNumber = "XXXX XXXX XXXX XXXX";
+  navigator.clipboard.writeText(cardNumber);
+  alert("Номер карты скопирован");
+}
